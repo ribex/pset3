5538df969d4863df6f69617e6309d4973ffc8b96
Rebecca Wagaman <ribexy@gmail.com>
2017-11-04 16:54:49 +0000

    Implement binary search - correct

--- find/helpers.c
+++ find/helpers.c
@@ -13,32 +13,39 @@
  */
 bool search(int value, int values[], int n)
 {
-    printf("Beginning search\n");
+    // printf("Beginning search\n");
     int left = 0;
-    int right = n;
+    int right = n - 1;
+
 // while length of list > 0
     do {
+        if (left > right)
+        {
+            return false;
+        }
+
 //      look at middle of list
 //      if number found, return true
-        if (value == values[right / 2])
+        if (value == values[(left + right) / 2])
         {
-            printf("%i ", values[right / 2]);
+            // printf("%i ", values[left + right / 2]);
             return true;
         }
 //      else if number higher, search left
-        else if (value > values[right / 2])
+        else if (value > values[(left + right) / 2])
         {
-            printf("old left = %i ", left);
-            left = (right / 2) + 1;
-            printf("new left = %i ", left);
+            // printf("old left = %i ", left);
+            left = ((left + right) / 2) + 1;
+            // printf("new left = %i ", left);
         }
 //      else if number lower, search right
-        else if (value < values[right / 2])
+        else if (value < values[(left + right) / 2])
         {
-            printf("old right = %i ", right);
-            right = (right / 2) - 1;
-            printf("new right = %i ", right);
+            // printf("old right = %i ", right);
+            right = ((left + right) / 2) - 1;
+            // printf("new right = %i ", right);
         }
+
     } while (n > 0);
 // return false
     return false;
@@ -51,7 +58,7 @@ void sort(int values[], int n)
 {
 
 // selection sort
-    printf("Beginning sort\n");
+    // printf("Beginning sort\n");
 // for i = 0 to n - 2
     for (int i = 0; i < n; i++)
     {
@@ -76,8 +83,8 @@ void sort(int values[], int n)
 
         }
     }
-    printf("Sort completed\n");
+    // printf("Sort completed\n");
     for (int k = 0; k < n; k++)
-    printf("%i ", values[k]);
+    // printf("%i ", values[k]);
     return;
 }
